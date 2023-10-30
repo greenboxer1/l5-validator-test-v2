4c545f0c5d3c7fe601ff7b192099a379c5debe7a
greenboxer1 <greenboxer@list.ru>
2023-10-31 00:28:25 +0300

    fix 3 steps

--- src/ArraySchema.js
+++ src/ArraySchema.js
@@ -1,6 +1,7 @@
 export default class ArraySchema {
-    validators = [(value) => Array.isArray(value)];
-    isValid(value) {
-        return this.validators.every((validator) => validator(value));
-    }
+  validators = [(value) => Array.isArray(value)];
+
+  isValid(value) {
+    return this.validators.every((validator) => validator(value));
+  }
 }
--- src/Validator.js
+++ src/Validator.js
@@ -1,11 +1,14 @@
-import NumberSchema from "./NumberSchema.js"
-import ArraySchema from "./ArraySchema.js";
+/* eslint-disable */
+import NumberSchema from './NumberSchema.js';
+import ArraySchema from './ArraySchema.js';
+import { ESLint } from 'eslint';
 
 export default class Validator {
-    string() {
-        return new NumberSchema();
-    }
-    array() {
-        return new ArraySchema()
-    }
+  string() {
+    return new NumberSchema();
+  }
+
+  array() {
+    return new ArraySchema();
+  }
 }
